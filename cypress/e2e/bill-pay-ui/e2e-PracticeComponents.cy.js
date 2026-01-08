e482f76328000fde4814ba4c5d5a4d2032c3a6e1
rezaparamarta <rezaparamarta30@gmail.com>
2026-01-08 17:42:11 +0700

    add preparations

---
+++ cypress/e2e/bill-pay-ui/e2e-PracticeComponents.cy.js
@@ -0,0 +1,89 @@
+describe('Bill Pay UI Practice Components', () => {
+    // Accessing URL Bill Pay UI
+    beforeEach(() => {
+        cy.visit('https://gauravkhurana.in/practise-api/ui/index.html#/practice');
+    });
+
+    // Input forms of profile
+    it('Input Basic Elements', () => {
+        cy.get('[data-testid="first-name"]').type('Gajah');
+        cy.get('[data-testid="email"]').type('gajahpred@gmail.com');
+        cy.get('[data-testid="password"]').type('planetk3pl3r22b');
+        cy.get('[data-testid="bio"]').type('I am a QA engineer and I love to test');
+        // Assertion of the input was filled
+        cy.get('#passwordStrength').should('contain', 'Strong');
+        cy.get('#passwordStrength').click();
+        // Choose any option from the dropdown
+        cy.get('[data-testid="country"]').select('India');
+
+        // Assertion of the dropdown was filled
+        cy.get('[data-testid="country"]').should('have.value', 'in');
+
+        // Choose any options from multiple option dropdown
+        cy.get('[data-testid="frameworks"]').select('Cypress');
+
+        // Assertion of the multiple dropdown was filled
+        cy.contains('Cypress').should('be.visible');
+        
+        // Choose any radio button option
+        cy.get('[data-testid="gender-male"]').click();
+
+        // choose any checkbox option
+        cy.get('[data-testid="terms"]').click();
+        cy.get('[data-testid="terms"]').should('be.checked');
+
+        // Choose any checkbox option
+        cy.get('[data-testid="notifications"]').click();
+        cy.get('[data-testid="notifications"]').should('be.checked');
+
+        cy.get('[data-testid="btn-primary"]').click();
+
+        // Assertion for checking Buttons (Various States) was clicked
+        cy.get('#btnOutput').should('contain', 'btnPrimary');
+        // Slide value volume into 80%
+        cy.get('[data-testid="volume-slider"]')
+          .invoke('val', 80)
+          .trigger('input')
+          .trigger('change');
+
+        // Assertion for checking Slider was filled
+        cy.get('[data-testid="volume-slider"]').should('have.value', 80);
+        cy.get('#volume').should('have.value', 80);
+
+        // Slide value price into 350$
+        cy.get('[data-testid="price-slider"]')
+          .invoke('val', 350)
+          .trigger('input')
+          .trigger('change');
+
+        // Assertion for checking Slider was filled
+        cy.get('[data-testid="price-slider"]').should('have.value', 350);
+        cy.get('#price').should('have.value', 350);
+
+
+        // Click date picker
+        cy.get('[data-testid="date-input"]')
+          .invoke('val', '2026-01-08')
+          .trigger('input')
+          .trigger('change');
+          
+        // Assertion for checking Date Picker was filled
+        cy.get('[data-testid="date-input"]').should('have.value', '2026-01-08');
+
+        // Choose Time
+        cy.get('[data-testid="time-input"]')
+          .invoke('val', '18:24')
+          .trigger('input')
+          .trigger('change');
+
+
+        // Choose date and time
+        cy.get('[data-testid="datetime-input"]')
+          .invoke('val', '2026-01-08 18:24')
+          .trigger('input')
+          .trigger('change');
+        
+        
+
+    })
+})
